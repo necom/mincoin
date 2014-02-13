60037830e680057993dc16cf30a7ee12928923d7
vipah <thevipah@hotmail.com>
2014-02-12 21:10:47 -0800

    Initial Commit

---
+++ src/qt/aboutdialog.h
@@ -0,0 +1,30 @@
+#ifndef ABOUTDIALOG_H
+#define ABOUTDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+    class AboutDialog;
+}
+class ClientModel;
+
+/** "About" dialog box */
+class AboutDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit AboutDialog(QWidget *parent = 0);
+    ~AboutDialog();
+
+    void setModel(ClientModel *model);
+
+private:
+    Ui::AboutDialog *ui;
+
+private slots:
+    void on_buttonBox_accepted();
+    void on_btnWebsiteLogo_pressed();
+};
+
+#endif // ABOUTDIALOG_H
